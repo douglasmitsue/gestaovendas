cd81d020fa4b24d57aadee3a2f577bcaa725e5a3
douglasmitsue <contato@douglasmitsue.com>
2022-01-04 22:34:51 -0300

    Atualizando categoria no banco de dados

--- src/main/java/com/douglasmitsue/gestaovendas/controlador/CategoriaControlador.java
+++ src/main/java/com/douglasmitsue/gestaovendas/controlador/CategoriaControlador.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglasmitsue.gestaovendas.entidades.Categoria;
@@ -22,21 +24,26 @@ public class CategoriaControlador {
 
 	@Autowired
 	private CategoriaServico categoriaServi1co;
-	
+
 	@GetMapping
-	public List<Categoria> listarTodas(){
+	public List<Categoria> listarTodas() {
 		return categoriaServi1co.listarTodas();
 	}
-	
+
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo){
-		 Optional<Categoria> categoria = categoriaServi1co.buscarPorId(codigo);
-		 return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo) {
+		Optional<Categoria> categoria = categoriaServi1co.buscarPorCodigo(codigo);
+		return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Categoria> salva(@RequestBody Categoria categoria){
+	public ResponseEntity<Categoria> salva(@RequestBody Categoria categoria) {
 		Categoria categoriaSalva = categoriaServi1co.salvar(categoria);
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @RequestBody Categoria categoria) {
+		return ResponseEntity.ok(categoriaServi1co.atualizar(codigo, categoria));
 	}
 }
